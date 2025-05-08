77a6d7861a0ee52eda8e546806b04c623d5357d5
Petr Kubica <cubicap@email.cz>
2025-05-08 03:53:46 +0200

    fix stackAllocator shadowing blk arguments

--- src/jac/machine/compiler/stackAllocator.h
+++ src/jac/machine/compiler/stackAllocator.h
@@ -13,10 +13,17 @@ inline std::map<TmpId, int> allocateStackSlots(jac::cfg::Function& cfg) {
     std::map<TmpId, int> stackSlots;
     int stackOffset = 0;
 
+    std::set<TmpId> args;
+    for (auto& arg : cfg.args) {
+        if (arg.type == ValueType::Any) {
+            args.insert(arg.id);
+        }
+    }
+
     for (auto& block : cfg.blocks) {
         for (auto& stmt : block->statements) {
             Temp res = stmt.res();
-            if (res.type == ValueType::Any) {
+            if (res.type == ValueType::Any && !args.contains(res.id)) {
                 stackSlots.emplace(res.id, stackOffset);
                 stackOffset++;
             }
